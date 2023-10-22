739a738998c112a901123e198203c9bf1297a7d3
AbuSolaimane <fadilimustafa530@gmail.com>
2023-10-22 04:17:11 -0700

    hjdjd

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+this is a module
+"""
+
+from flask import Flask
+
+app = Flask("__name__")
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """this a function"""
+    return ("Hello HBNB!")
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """this a function"""
+    return ("HBNB")
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def descripe_c(text):
+    """this a function"""
+    return ("C {}".format(text.replace("_", " ")))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
