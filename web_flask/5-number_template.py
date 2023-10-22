7bfebcef464cfc8b87559b8a0af9553988881d10
AbuSolaimane <fadilimustafa530@gmail.com>
2023-10-22 04:53:25 -0700

    hjdjd

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""
+this is a module
+"""
+
+from flask import Flask, render_template
+
+app = Flask("__name__")
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """this a function"""
+    return ("Hello HBNB!")
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """this a function"""
+    return ("HBNB")
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def descripe_c(text):
+    """this a function"""
+    return ("C {}".format(text.replace("_", " ")))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def descripe_python(text="is cool"):
+    """this a function"""
+    return ("Python {}".format(text.replace("_", " ")))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """this a function"""
+    return ("{} is a number".format(n))
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    """this a function"""
+    return (render_template("5-number.html", number=n))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
